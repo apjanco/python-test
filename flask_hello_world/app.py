48637bc8eb921306b28df38d934db89ea7db1c5e
Andrew Janco <ajanco@haverford.edu>
2016-06-30 14:41:27 -0400

    Dynamic routes

--- flask_hello_world/app.py
+++ flask_hello_world/app.py
@@ -5,10 +5,39 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
+def hello_world():
+	return "Hello, World!!!!"
+	
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+	
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1
+	return "correct"
 
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1
+	
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct" 
+	
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael" :
+		return "Hello, {}".format(name), 200
+	else :
+		return "Not found", 404
+		
 # define the view using a function, withic returns a string
 def hello_world():
 	return "Hello, World!"
